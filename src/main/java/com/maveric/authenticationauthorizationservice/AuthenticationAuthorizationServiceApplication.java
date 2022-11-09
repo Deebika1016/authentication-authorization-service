27cc6fafab8cfc5334487e80f79e2d06e22e177a
Deebika1016 <Deebikakt@maveric-systems.com>
2022-11-09 10:26:51 +0530

    final commit

--- src/main/java/com/maveric/authenticationauthorizationservice/AuthenticationAuthorizationServiceApplication.java
+++ src/main/java/com/maveric/authenticationauthorizationservice/AuthenticationAuthorizationServiceApplication.java
@@ -1,5 +1,6 @@
 package com.maveric.authenticationauthorizationservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -13,8 +14,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableEurekaClient
 public class AuthenticationAuthorizationServiceApplication {
 
+	@Value("${frontend.uri}")
+	private String frontendUri;
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationAuthorizationServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(frontendUri);
+			}
+		};
 	}
 
 }
--- src/main/java/com/maveric/authenticationauthorizationservice/util/JwtUtil.java
+++ src/main/java/com/maveric/authenticationauthorizationservice/util/JwtUtil.java
@@ -3,6 +3,7 @@ package com.maveric.authenticationauthorizationservice.util;
 import com.maveric.authenticationauthorizationservice.dto.GateWayResponseDto;
 import com.maveric.authenticationauthorizationservice.model.UserPrincipal;
 import io.jsonwebtoken.*;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,8 @@ import java.util.function.Function;
 
 @Service
 public class JwtUtil {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(JwtUtil.class);
     @Value("${jwt.secret}")
     private String secretKey;
 
@@ -38,13 +41,13 @@ public class JwtUtil {
 
     public String generateToken(UserPrincipal userDetails) {
         Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, userDetails.getUsername());
+        return createToken(claims, userDetails.getUsername(),userDetails.getUser().get_id());
     }
 
-    private String createToken(Map<String, Object> claims, String subject) {
-
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+    private String createToken(Map<String, Object> claims, String subject,String id) {
+        log.info("Token creation!");
+        return Jwts.builder().setClaims(claims).setId(id).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 7)) // 7 mins
                 .signWith(SignatureAlgorithm.HS256, secretKey).compact();
     }
 
@@ -52,9 +55,9 @@ public class JwtUtil {
         try {
             Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
             return new GateWayResponseDto(true,extractAllClaims(token));
-        } catch (SignatureException | MalformedJwtException | ExpiredJwtException | UnsupportedJwtException | IllegalArgumentException e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            return new GateWayResponseDto(false,null);
         }
-        return new GateWayResponseDto(false,null);
+
     }
 }
